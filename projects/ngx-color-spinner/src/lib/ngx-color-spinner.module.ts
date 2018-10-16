0012b16423e577b6786fd02c9c13e57a20deae3d
Partha Ranjan Nayak <partharanjan.nayak@epiqglobal.com>
2018-10-16 17:57:37 +0530

    first released

--- projects/ngx-color-spinner/src/lib/ngx-color-spinner.module.ts
+++ projects/ngx-color-spinner/src/lib/ngx-color-spinner.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { NgxColorSpinnerComponent } from './ngx-color-spinner.component';
+import { MatProgressSpinnerModule } from '@angular/material'
 
 @NgModule({
   imports: [
+    MatProgressSpinnerModule
   ],
   declarations: [NgxColorSpinnerComponent],
   exports: [NgxColorSpinnerComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { NgxColorSpinnerModule } from 'projects/ngx-color-spinner/src/public_api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxColorSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
